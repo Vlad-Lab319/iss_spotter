4d4df2cffd8473162a4d5f6ca729a0ea2ccf8df9
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-03 00:45:27 +0000

    Returns IP

--- iss.js
+++ iss.js
@@ -1,1 +1,14 @@
+const request = require('request');
 
+const fetchMyIP = function(callback) { 
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response ,ip) => {
+    
+    callback(error, ip);
+  });
+  
+}
+  
+
+
+module.exports = { fetchMyIP };
